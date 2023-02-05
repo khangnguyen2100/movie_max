592ad4ac982efca090e9a6eada593326016fa86d
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-02-05 21:57:28 +0700

    enhance 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,37 +1,36 @@
-import React from 'react'
-import { Box, Flex, Text, Heading } from '@chakra-ui/react'
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 const NotFound = () => {
   return (
-    <Flex direction={'column'} align='center' justify={'center'} mt='50px'>
-      <Heading
-        fontWeight={'bold'}
-        fontSize='120px'
-        lineHeight={'1'}
-      >
-        404
-      </Heading>
-      <Text
-        fontSize={'36px'}
-        fontWeight='600'
-      >
-        Page not found
-      </Text>
-      <Box
-        fontSize={'20px'}
-        fontWeight='500'
-      >
-        Please go back to our home page at&nbsp;
+    <Flex align='center' justify={'center'} mt='30px'>
+      {/* image */}
+      <Box w={'50%'}>
+        <Image  srcSet='/images/page-not-found.png' loading='eager' src='/images/page-not-found.png' alt='404' />
+      </Box>
+      {/* content */}
+      <Box ml={'50px'}>
         <Text
-          color='primaryColor'
-          display={'inline-block'}
+          fontSize={'38px'}
+          fontWeight='700'
         >
-          <Link
-            to={'/'}
-          >
-            here!
-          </Link>
+          Page Not Found
         </Text>
+        <Box
+          fontSize={'20px'}
+          fontWeight='500'
+        >
+          Please go back to our home page at&nbsp;
+          <Text
+            color='primaryColor'
+            display={'inline-block'}
+          >
+            <Link
+              to={'/'}
+            >
+              here!
+            </Link>
+          </Text>
+        </Box>
       </Box>
     </Flex>
   )
