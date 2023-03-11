41c50e3a0ebfef02c83834348c551f07225aae6a
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-03-11 14:27:33 +0700

    fix css bug when using yarn

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box } from "@chakra-ui/react";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { ThreeDots } from "react-loader-spinner";
 import { colors } from "../../utils/themeConfig";
 const Loading = () => {
