14867ef5c2b416ff7738a226e817406f6f167393
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-03-11 14:30:04 +0700

    use jsx for react components

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,72 @@
+import React, { memo } from "react";
+import { Link } from "react-router-dom";
+import { Box, Flex, Heading } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Keyboard } from "swiper";
+import "swiper/css";
+
+import ButtonBg from "../Buttons/ButtonBg";
+import Film from "../Film/Film";
+import { getConfigSelector } from "../../redux/selector";
+import { useSelector } from "react-redux";
+const Section = ({ data = [], name }) => {
+  const { config } = useSelector(getConfigSelector);
+
+  return (
+    <Box mb="50px">
+      <Flex mb="30px" justify="space-between" align="center">
+        <Heading
+          textTransform="capitalize"
+          fontSize={{
+            base: "xl",
+            md: "2xl",
+          }}
+        >
+          {data?.homeSectionName || name}
+        </Heading>
+        <Link to={`/collection/${data?.homeSectionId}`}>
+          <ButtonBg>
+            More
+            <ArrowForwardIcon ml={2} />
+          </ButtonBg>
+        </Link>
+      </Flex>
+
+      <Swiper
+        slidesPerView={3.2}
+        spaceBetween={15}
+        breakpoints={{
+          768: {
+            slidesPerView: 4.3,
+          },
+          922: {
+            slidesPerView: 6.3,
+          },
+        }}
+        keyboard={true}
+        modules={[Keyboard]}
+      >
+        {data?.map((data, i) => {
+          if (i < 18) {
+            return (
+              <SwiperSlide key={i}>
+                <Film
+                  baseUrl={`${config?.images?.base_url}/original/`}
+                  media_type={data.media_type}
+                  id={data.id}
+                  vote_average={data.vote_average}
+                  poster_path={data.poster_path}
+                  title={data.title}
+                  name={data.name}
+                />
+              </SwiperSlide>
+            );
+          }
+        })}
+      </Swiper>
+    </Box>
+  );
+};
+
+export default memo(Section);
---
+++ src/pages/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Flex } from '@chakra-ui/react'
+
+import { searchKeyWordsSelector } from '../redux/selector'
+import ListFilmLayout from '../components/Layout/ListFilmLayout'
+export const Search = () => {
+  let data = useSelector(searchKeyWordsSelector)
+  return (
+    <Flex justify={'space-between'} align>  
+      <ListFilmLayout listFilm={data} />
+    </Flex>
+  )
+}
