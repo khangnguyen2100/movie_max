e2b81a0cd892b13457c4a01a37130239352755a0
khangnguyen2100 <khangnguyen23112100@gmail.com>
2023-03-25 16:46:58 +0700

    enhance btn

--- src/components/Buttons/ButtonWhite.jsx
+++ src/components/Buttons/ButtonWhite.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
 import { Box } from '@chakra-ui/react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 const ButtonWhite = ({ href = "/", children, customStyle }) => {
   return (
     <Box
       display='block' h='full' px='16'
-      bgColor={'transparent'} color={'#FCE38A'} border={'1px solid #FCE38A'}
+      bgColor={'transparent'} color={'#fff'} border={'1px solid rgba(50, 138, 241, 1)'}
       textTransform={'uppercase'} fontSize={'18px'} textAlign={'center'} fontWeight='bold' letterSpacing='3px'
       rounded='sm' cursor='pointer'
       transition={'all 0.2s ease'}
       boxShadow='md'
       role="group"
+      bg={'rgba(50, 138, 241, .8)'}
       pos={'relative'}
       overflow={'hidden'}
       _hover={{
-        color: '#333',
+        color: '#fff',
         transition: 'all 0.3s ease-in-out'
       }}
       {...customStyle}
     >
       <Box
         w={0} h={'100%'} pos={'absolute'} top={0} left={'-15px'}
-        bgColor={'#FCE38A'}
+        bgColor={'rgba(50, 138, 241, 1)'}
         transition={'all 0.35s ease-out'}
         _groupHover={{ width: '110%' }}
         zIndex={-1}
-        transform={'skewX(-20deg)'}
+        transform={'skewX(-30deg)'}
       />
       <Link
         to={href}
